41f7faebd23073f4b8500eb0582058c28aa6a465
dan ajayi <56173275+dan-builds-robots@users.noreply.github.com>
2022-05-21 21:22:23 -0400

    Add files via upload

--- Prediction/ExtractiveSummarizer.ts
+++ Prediction/ExtractiveSummarizer.ts
@@ -1,19 +1,49 @@
+import { Tokenizer } from './Tokenizer.js';
+import { Collator } from './Collator';
+import { ICollator } from './ICollator';
 import { 
     IExtractiveSummarizer,
-    ExtractiveSummarizerResponse, 
     Span 
 } from './IExtractiveSummarizer';
+import { CLIPModel } from './CLIPModel.js';
+import { spawnSync } from 'child_process';
 
 
 export class CLIPSummarizer implements IExtractiveSummarizer {
-    
-    async extract(document: string): Promise<ExtractiveSummarizerResponse> {
-        // Separate into individual sentences
+    collator: ICollator;
+    model: CLIPModel;
 
-        // Collate 
+    constructor(
+        tokenizerPath: string,
+        bertPath: string,
+        classifierPath: string,
+    ) {
+        let tokenizer = Tokenizer.fromFile(tokenizerPath);
+        this.collator = new Collator(tokenizer);
+        this.model = new CLIPModel(bertPath, classifierPath);
+    }
+    
+    async extract(
+        document: string,
+        nContextSentences: number = 2,
+    ): Promise<Span[]> {
+        // Separate into individual sentences
+        let sentence_spans = this.splitDocument(document);
+        let sentences = sentence_spans
+            .map((span) => document.substring(span.start, span.end));
+
+        // prepare inputs
+        let model_inputs = await this.collator.collate(sentences, nContextSentences);
 
         // Query to Model
-        throw Error("Not Implemented");
+        let output: Promise<Span[]> = Promise.all(sentence_spans
+            .map(async (span, idx) => ({
+                start: span.start,
+                end: span.end,
+                label: await this.model.forward(model_inputs[idx])
+            }))
+        );
+        return output;
     }
 
     /**
@@ -24,19 +54,6 @@ export class CLIPSummarizer implements IExtractiveSummarizer {
      */
     private splitDocument(document: string): Span[] {
         // TODO: Daniel
-        let sentences = [];
-        let start = 0;
-
-        for (let i = 0; i < document.length; i++) {
-            if (document[i] == '.') {
-                if (document.slice(i - 2, i) in ['Mr', 'Dr'] || document.slice(i - 3, i) == 'Mrs') {
-                    continue;
-                } else {
-                    sentences.push([start, i]);
-                }
-            }
-        }
-
-        return sentences;
+        throw Error("Not Implemented");
     }
 }
